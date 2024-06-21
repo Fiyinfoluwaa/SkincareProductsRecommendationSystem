96b04529298094729fba270a32239a396d48ebe1
Olaleye Elizabeth <olaleyefiyin@gmail.com>
2024-06-21 14:07:51 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Skincare Products Recommender",
+                   layout="wide")
+
+st.title("Skincare Products Recommendation System")
+st.sidebar.success("select a page above")
